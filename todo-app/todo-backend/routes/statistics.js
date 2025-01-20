877a43a008f2d5a8f15df86d44aac1cbae620c47
ongThien <ongthiendev@gmail.com>
2025-01-21 00:58:22 +0700

    finished Exercise 12.12: Persisting data in Redis

--- todo-app/todo-backend/routes/statistics.js
+++ todo-app/todo-backend/routes/statistics.js
@@ -8,11 +8,11 @@ const { ADDED_TODOS_KEY } = require('../util/constants');
 router.get('/', async (req, res) => {
   const addedTodos = await redis.getAsync(ADDED_TODOS_KEY);
 
-  if (!addedTodos) {
-    const currentTodos = await Todo.find({});
-    await redis.setAsync(ADDED_TODOS_KEY, currentTodos.length)
-    return res.status(200).send(JSON.stringify({ added_todos: currentTodos.length }));
-  }
+  // if (!addedTodos) {
+  //   const currentTodos = await Todo.find({});
+  //   await redis.setAsync(ADDED_TODOS_KEY, currentTodos.length)
+  //   return res.status(200).send({ added_todos: currentTodos.length });
+  // }
 
   res.status(200).send({ added_todos: addedTodos });
 });
